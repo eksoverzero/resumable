434185d5b4c65a0cc91dea38c60454d93a281d3c
Steven Eksteen <eksoverzero@outlook.com>
2018-06-04 13:08:53 +0200

    Flask init

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.use(express.static('public'))
-
-// app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
