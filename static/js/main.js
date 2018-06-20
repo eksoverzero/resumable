0051fb095f2f3ee66e27f29e5495114485a0e318
Steven Eksteen <eksoverzero@outlook.com>
2018-06-20 16:29:24 +0200

    Bring down chunck sizre

--- static/js/main.js
+++ static/js/main.js
@@ -2,7 +2,7 @@ $(document).ready( function() {
   $('.dropzone').ṛesumable({
     query: { path: '10/1' },
     target: '/upload',
-    chunkSize: 2*1024*1024,
+    chunkSize: 1*1024*1024,
     simultaneousUploads: 4,
     prioritizeFirstAndLastChunk: true
   });
