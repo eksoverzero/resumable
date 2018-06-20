a65bcbb1cd1edb25a6ca88f3a6eec881bb635d0f
Steven Eksteen <eksoverzero@outlook.com>
2018-06-20 22:09:10 +0200

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -2,7 +2,7 @@ $(document).ready( function() {
   $('.dropzone').ṛesumable({
     query: { path: '10/1' },
     target: '/upload',
-    chunkSize: 1*1024*1024,
+    chunkSize: 128*1024,
     simultaneousUploads: 2,
     prioritizeFirstAndLastChunk: true
   });
