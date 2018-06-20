a30060934eefd00938658ab82120111c252ae5ea
Steven Eksteen <eksoverzero@outlook.com>
2018-06-21 01:36:46 +0200

    Incomplete

--- handler.js
+++ handler.js
@@ -1,52 +1,76 @@
 'use strict';
 
 const multipart = require('aws-lambda-multipart-parser');
-const fileExists = require('./lib/fileExists');
+const checkFile = require('./lib/checkFile');
+const createFile = require('./lib/createFile');
+const mergeFiles = require('./lib/mergeFiles');
+const getFilename = require('./lib/getFilename');
 const validateRequest = require('./lib/validateRequest');
+const getChunkFilename = require('./lib/getChunkFilename');
 
 module.exports.get = (event, context, callback) => {
   const params = event.queryStringParameters;
-  const filename = params.resumableFilename,
-        chunkSize = parseInt(params.resumableChunkSize),
-        totalSize = parseInt(params.resumableTotalSize),
-        identifier = params.resumableIdentifier,
-        chunkNumber = parseInt(params.resumableChunkNumber),
-        numberOfChunks = Math.max(Math.floor(totalSize/(chunkSize*1.0)), 1);
 
-  return validateRequest(chunkNumber, chunkSize, totalSize, identifier, filename)
-         .then(function(message) {
-           return fileExists(filename);
-         })
-         .then(function(filename) {
-           callback(null, {
-             statusCode: 200,
-             // headers: { 'Content-Type': 'application/json' },
-             body: filename,
-           });
-         })
-         .catch(function(error) {
-           callback(null, {
-             statusCode: 404,
-             // headers: { 'Content-Type': 'application/json' },
-             body: error,
-           });
-         });
-};
-
-module.exports.post = (event, context, callback) => {
-  const params = multipart.parse(event, false)
-  const file = params.file,
+  const folder = params.path,
         filename = params.resumableFilename,
         chunkSize = parseInt(params.resumableChunkSize),
         totalSize = parseInt(params.resumableTotalSize),
         identifier = params.resumableIdentifier,
         chunkNumber = parseInt(params.resumableChunkNumber),
         numberOfChunks = Math.max(Math.floor(totalSize/(chunkSize*1.0)), 1);
-  // return validateRequest(chunkNumber, chunkSize, totalSize, identifier, filename, fileSize)
 
-  callback(null, {
-    statusCode: 404,
-     // headers: { 'Content-Type': 'application/json' },
-    body: file,
-  });
+  const Filename = getFilename(filename, folder);
+  const chunkFilename = getChunkFilename(Filename, chunkNumber);
+
+  return checkFile(chunkFilename)
+         .then(mergeFiles(Filename, numberOfChunks))
+         .then(function(filename) {
+           callback(null, {
+             statusCode: 200,
+             headers: { 'Content-Type': 'application/json' },
+             body: filename
+           });
+         })
+         .catch(function(error) {
+           callback(null, {
+             statusCode: 404,
+             headers: { 'Content-Type': 'application/json' },
+             body: error
+           });
+         });
+};
+
+module.exports.post = (event, context, callback) => {
+  const params = multipart.parse(event, false);
+
+  const file = params.file,
+        folder = params.path,
+        filename = params.resumableFilename,
+        chunkSize = parseInt(params.resumableChunkSize),
+        totalSize = parseInt(params.resumableTotalSize),
+        identifier = params.resumableIdentifier,
+        chunkNumber = parseInt(params.resumableChunkNumber),
+        numberOfChunks = Math.max(Math.floor(totalSize/(chunkSize*1.0)), 1);
+
+  const Filename = getFilename(filename, folder);
+  const chunkFilename = getChunkFilename(Filename, chunkNumber);
+
+  return createFile(chunkFilename, file)
+         .then(function(filename) {
+           return mergeFiles(filename, numberOfChunks);
+         })
+         .then(function(filename) {
+           callback(null, {
+             statusCode: 200,
+             headers: { 'Content-Type': 'application/json' },
+             body: filename
+           });
+         })
+         .catch(function(error) {
+           callback(null, {
+             statusCode: 404,
+             headers: { 'Content-Type': 'application/json' },
+             body: error
+           });
+         });
 };
--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,8 @@ $(document).ready( function() {
   $('.dropzone').ṛesumable({
     query: { path: '10/1' },
     target: 'http://localhost:3000/upload',
-    chunkSize: 2*1024*1024,
-    simultaneousUploads: 4,
+    chunkSize: 128*1024,
+    simultaneousUploads: 2,
     prioritizeFirstAndLastChunk: true
   });
 });
