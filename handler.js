705bfe1b1d4addd0f757b093e24a8bd7d0d62d22
Steven Eksteen <eksoverzero@outlook.com>
2018-06-22 00:54:27 +0200

    Incomplete file merge after chunck upload

--- handler.js
+++ handler.js
@@ -4,11 +4,9 @@ const multipart = require('aws-lambda-multipart-parser');
 const checkFile = require('./lib/checkFile');
 const createFile = require('./lib/createFile');
 const mergeFiles = require('./lib/mergeFiles');
-const getFilename = require('./lib/getFilename');
-const validateRequest = require('./lib/validateRequest');
 const getChunkFilename = require('./lib/getChunkFilename');
 
-module.exports.get = (event, context, callback) => {
+module.exports.resumableGET = (event, context, callback) => {
   const params = event.queryStringParameters;
 
   const folder = params.path,
@@ -19,28 +17,37 @@ module.exports.get = (event, context, callback) => {
         chunkNumber = parseInt(params.resumableChunkNumber),
         numberOfChunks = Math.max(Math.floor(totalSize/(chunkSize*1.0)), 1);
 
-  const Filename = getFilename(filename, folder);
-  const chunkFilename = getChunkFilename(Filename, chunkNumber);
+  const chunkFilename = getChunkFilename(filename, folder, chunkNumber);
 
   return checkFile(chunkFilename)
-         .then(mergeFiles(Filename, numberOfChunks))
          .then(function(filename) {
-           callback(null, {
-             statusCode: 200,
-             headers: { 'Content-Type': 'application/json' },
-             body: filename
-           });
+           return mergeFiles(filename, numberOfChunks);
+         })
+         .then(function(filename) {
+           if(typeof(filename)!='undefined') {
+             callback(null, {
+               statusCode: 200,
+               headers: { 'Content-Type': 'application/json' },
+               body: filename
+             });
+           } else {
+             callback(null, {
+               statusCode: 204,
+               headers: { 'Content-Type': 'application/json' },
+               body: error
+             });
+           }
          })
          .catch(function(error) {
            callback(null, {
-             statusCode: 404,
+             statusCode: 500,
              headers: { 'Content-Type': 'application/json' },
              body: error
            });
          });
 };
 
-module.exports.post = (event, context, callback) => {
+module.exports.resumablePOST = (event, context, callback) => {
   const params = multipart.parse(event, false);
 
   const file = params.file,
@@ -52,13 +59,9 @@ module.exports.post = (event, context, callback) => {
         chunkNumber = parseInt(params.resumableChunkNumber),
         numberOfChunks = Math.max(Math.floor(totalSize/(chunkSize*1.0)), 1);
 
-  const Filename = getFilename(filename, folder);
-  const chunkFilename = getChunkFilename(Filename, chunkNumber);
+  const chunkFilename = getChunkFilename(filename, folder, chunkNumber);
 
   return createFile(chunkFilename, file)
-         .then(function(filename) {
-           return mergeFiles(filename, numberOfChunks);
-         })
          .then(function(filename) {
            callback(null, {
              statusCode: 200,
--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready( function() {
   $('.dropzone').ṛesumable({
     query: { path: '10/1' },
-    target: 'http://localhost:3000/upload',
+    target: 'http://localhost:3000/resumable',
     chunkSize: 128*1024,
     simultaneousUploads: 2,
     prioritizeFirstAndLastChunk: true
