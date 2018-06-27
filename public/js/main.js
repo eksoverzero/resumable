14f04966c6ac865693a1e8dc542c73b791bc686d
Steven Eksteen <eksoverzero@outlook.com>
2018-06-27 20:02:38 +0200

    CORS

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready( function() {
   $('.dropzone').ṛesumable({
     query: { path: '10/1' },
-    target: 'http://localhost:3000/resumable',
+    target: 'https://dodjy00g87.execute-api.eu-west-1.amazonaws.com/dev/resumable',
     chunkSize: 128*1024,
     simultaneousUploads: 2,
     prioritizeFirstAndLastChunk: true
