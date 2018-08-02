f7c495e887b472cabbe547853fde51c00978e130
Steven Eksteen <eksoverzero@outlook.com>
2018-08-02 18:27:00 +0200

    Start script

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready( function() {
   $('.dropzone').ṛesumable({
     query: { path: '10/1' },
-    target: 'http://localhost:3000/resumable',
+    target: 'https://cloudburstuploads.herokuapp.com/resumable',
     fileType: ['gif', 'jpg', 'png', 'jpeg'],
     chunkSize: 128*1024,
     simultaneousUploads: 2,
